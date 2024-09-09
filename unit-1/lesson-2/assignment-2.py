835a1fcbc74eb2cc0724eb4dec14c1e9168ba412
a. nani williams <a.naniwilliams@as-macbook-air.wireless.newschool.edu>
2024-09-09 15:44:20 -0400

    this is the firat comment

---
+++ unit-1/lesson-2/assignment-2.py
@@ -0,0 +1,3 @@
+from unit1lesson2 import *
+number_list.sort()
+print(number_list)
---
+++ unit-1/lesson-2/classwork.py
@@ -0,0 +1,19 @@
+score = 28
+# this is a message, the computer won't read
+name = "lu"
+print (name)
+name = "miscel"
+print (name)
+
+count = 0
+print(count)
+count = count + 1
+print (count)
+
+names = ["Badah", "Beimnet", "Kirsten", "Cate"]
+numberofnames = len(names)
+print(numberofnames)
+print(names[0])
+
+for name in names:
+    print(name)
