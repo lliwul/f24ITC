284d94b14a7c9f0b9dd68c002c88e47fbd3e28ef
a. nani williams <a.naniwilliams@as-macbook-air.wireless.newschool.edu>
2024-09-18 16:14:57 -0400

    all done

--- unit-1/lesson-3/newestfile.py
+++ unit-1/lesson-3/newestfile.py
@@ -4,8 +4,9 @@ from PIL import Image
 if len(sys.argv) != 3:
     print("Error: Please provide two image filenames.")
     sys.exit(1)
+img1 = Image.open(sys.argv[1])
+img2 = Image.open(sys.argv[2])
 
 blended_img = Image.blend(img1,img2,.5)
 
-img1 = Image.open(sys.argv[1])
-img2 = Image.open(sys.argv[2])
+
